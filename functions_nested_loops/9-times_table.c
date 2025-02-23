1d7ee4af714d138eac96b2533ecb33b6e35e8cef
EzequielKH <laudarosa12@gmail.com>
2025-02-23 19:03:41 +0000

    arreglado syntax

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,27 +9,30 @@ void times_table(void)
 {
 	int i, j, product;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)  /* Loop through rows (0 to 9) */
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)  /* Loop through columns (0 to 9) */
 		{
 			product = i * j;
 
-			if (j != 0)
-				_putchar(',');
-
-			if (product < 10)
+			if (j != 0)  /* To avoid leading space for the first number */
 			{
-				if (j != 0)
-					_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (product < 10)  /* Single digit numbers */
+			{
+				if (j != 0)  /* Print a space for alignment */
+				_putchar(' ');
 				_putchar('0' + product);
 			}
-			else
+			else  /* Double digit numbers */
 			{
-				_putchar('0' + (product / 10));
-				_putchar('0' + (product % 10));
+				_putchar('0' + (product / 10));  /* Tens place */
+				_putchar('0' + (product % 10));  /* Ones place */
 			}
 		}
-		_putchar('\n');
+		_putchar('\n');  /* Newline after each row */
 	}
 }
