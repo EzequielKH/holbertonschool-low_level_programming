7799f3aac4200bc4361599904d45722b760bfc47
EzequielKH <laudarosa12@gmail.com>
2025-03-08 22:06:22 +0000

    nuevo agregado u/o arreglado

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number to find the square root of
  *
- * Return: the natural square root of n, 
+ * Return: the natural square root of n,
  * or -1 if n does not have a natural square root
  */
 int _sqrt_recursion(int n)
