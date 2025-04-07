5a1ed3f99fe91bfbabad042d3aaaaf01dfac0264
EzequielKH <laudarosa12@gmail.com>
2025-04-07 16:43:40 +0000

    nuevo agregado u/o arreglado

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,7 +14,6 @@ void close_file(int fd)
 	if (close(fd) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd);
-		printf("100\n");
 		exit(100);
 	}
 }
@@ -34,31 +33,30 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		printf("97\n");
 		exit(97);
 	}
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		printf("98\n");
 		exit(98);
 	}
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		printf("99\n");
 		close_file(fd_from);
 		exit(99);
 	}
+
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
 		if (write_bytes != read_bytes)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			printf("99\n");
 			close_file(fd_from);
 			close_file(fd_to);
 			exit(99);
@@ -67,11 +65,11 @@ int main(int argc, char **argv)
 	if (read_bytes == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		printf("98\n");
 		close_file(fd_from);
 		close_file(fd_to);
 		exit(98);
 	}
+
 	close_file(fd_from);
 	close_file(fd_to);
 	return (0);
