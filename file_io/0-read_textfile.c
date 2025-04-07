1853fb174965268ef0df4321ee4f56abe964e1ef
EzequielKH <laudarosa12@gmail.com>
2025-04-07 16:32:05 +0000

    nuevo agregado u/o arreglado

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,11 +5,13 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file
+ * and prints it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
- * 
- * Return: The actual number of letters it could read and print, or 0 on failure.
+ *
+ * Return: The actual number of letters it could read and print,
+ * or 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -20,20 +22,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	/* Open the file for reading */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	/* Allocate memory for the buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
-	/* Read from the file */
 	read_bytes = read(fd, buffer, letters);
 	if (read_bytes == -1)
 	{
@@ -41,8 +39,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
-	/* Write to the standard output */
 	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
 	if (write_bytes == -1 || write_bytes != read_bytes)
 	{
@@ -50,8 +46,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
-	/* Clean up */
 	free(buffer);
 	close(fd);
 
--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
 #define BUFFER_SIZE 1024
 
 /**
@@ -29,7 +28,6 @@ void error_exit(int code, char *message)
 	dprintf(2, "%s\n", message);
 	exit(code);
 }
-
 /**
  * main - Copies content from one file to another.
  * @argc: The number of arguments.
@@ -42,29 +40,27 @@ int main(int argc, char **argv)
 	int fd_from, fd_to, read_bytes, write_bytes;
 	char buffer[BUFFER_SIZE];
 
-	/* Check if the number of arguments is correct */
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to");
 
-	/* Open the source file */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		char error_message[128];
-		snprintf(error_message, sizeof(error_message), "Error: Can't read from file %s", argv[1]);
+
+		snprintf(error_message, sizeof(error_message),
+				"Error: Can't read from file %s", argv[1]);
 		error_exit(98, error_message);
 	}
-
-	/* Open (or create) the destination file */
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		char error_message[128];
-		snprintf(error_message, sizeof(error_message), "Error: Can't write to file %s", argv[2]);
+
+		snprintf(error_message, sizeof(error_message),
+				"Error: Can't write to file %s", argv[2]);
 		error_exit(99, error_message);
 	}
-
-	/* Copy the content from file_from to file_to */
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
@@ -72,10 +68,7 @@ int main(int argc, char **argv)
 			error_exit(99, "Error: Can't write to file");
 
 	}
-
-	/* Close both file descriptors */
 	close_file(fd_from);
 	close_file(fd_to);
-
 	return (0);
 }
