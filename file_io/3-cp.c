748eaa13d5d8057fb790b0c444185b1749a9d34b
EzequielKH <laudarosa12@gmail.com>
2025-04-07 16:41:42 +0000

    nuevo agregado u/o arreglado

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,27 +14,17 @@ void close_file(int fd)
 	if (close(fd) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd);
+		printf("100\n");
 		exit(100);
 	}
 }
 
 /**
- * error_exit - Prints an error message and exits with a given code.
- * @code: The exit code to return.
- * @message: The error message to print.
- */
-void error_exit(int code, char *message)
-{
-	dprintf(2, "%s\n", message);
-	exit(code);
-}
-
-/**
  * main - Copies content from one file to another.
- * @argc: The number of arguments.
- * @argv: The arguments passed to the program.
+ * @argc: Argument count.
+ * @argv: Argument vector.
  *
- * Return: Always 0 (success).
+ * Return: 0 on success, exits with specific codes on failure.
  */
 int main(int argc, char **argv)
 {
@@ -42,18 +32,24 @@ int main(int argc, char **argv)
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to");
-
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		printf("97\n");
+		exit(97);
+	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		printf("98\n");
 		exit(98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		printf("99\n");
+		close_file(fd_from);
 		exit(99);
 	}
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
@@ -62,12 +58,18 @@ int main(int argc, char **argv)
 		if (write_bytes != read_bytes)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			printf("99\n");
+			close_file(fd_from);
+			close_file(fd_to);
 			exit(99);
 		}
 	}
 	if (read_bytes == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		printf("98\n");
+		close_file(fd_from);
+		close_file(fd_to);
 		exit(98);
 	}
 	close_file(fd_from);
