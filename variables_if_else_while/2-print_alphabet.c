25f28077c9ab1949ac04984cd43300cea37296c6
EzequielKH <laudarosa12@gmail.com>
2025-06-24 15:40:23 +0000

    test3

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return Always 0 (success)
+ */
 
 int main()
 {
+	char alfabeto = 'a';
 
-
-
+	while (alfabeto  <= 'z')
+	{
+		putchar(alfabeto);
+		alfabeto++;
+	}
+	putchar('\n');
 	return (0);
 }
