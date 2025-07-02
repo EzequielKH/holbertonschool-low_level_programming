281267845631afeade2c2b5a4ea3b42ebbb8ba7c
EzequielKH <laudarosa12@gmail.com>
2025-07-02 17:30:22 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,3 +7,20 @@
  * Return: always 0 (success)
  */
 
+void more_numbers(void)
+{
+	int num;
+	int veces;
+	for (veces = 0; veces < 10; veces++)
+	{
+		for (num = 0; num <= 14; num++){
+			
+				if (num >= 10)
+				{
+					_putchar((num / 10) + '0');
+				}
+				_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
