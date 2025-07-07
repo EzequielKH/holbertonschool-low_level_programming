45df8f0afab0441994853ee5b05e2f41c29768df
EzequielKH <laudarosa12@gmail.com>
2025-07-07 17:40:09 +0000

    thingy_added

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
 /**
- *
- *
+ * *_strcpy - entry point
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src);
 {
+	int len, i;
 
+	len = 0;
 
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
