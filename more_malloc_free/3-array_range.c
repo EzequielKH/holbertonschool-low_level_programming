c7a2290f3fe74da5ec40bf0bd76ca5c6d51e8e72
EzequielKH <laudarosa12@gmail.com>
2025-07-29 13:00:47 +0000

    messi

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -8,8 +8,15 @@
 
 int *array_range(int min, int max)
 {
+	int a;
+	int *p;
 
-
-
-
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * (max - min + 1));
+	if (p == 0)
+		return (NULL);
+	for (a = 0; min <= max; min++, a++)
+		p[a] = min;
+	return (p);
 }
