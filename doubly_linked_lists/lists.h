151d3c6893a557c89f28f7d160940d58bc87e834
EzequielKH <laudarosa12@gmail.com>
2025-08-05 13:20:54 +0000

    wa

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -13,13 +13,15 @@
  *
  * Description: node structure for a doubly linked list
  */
+
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
-int sum_dlistint(dlistint_t *head)
+
+int sum_dlistint(dlistint_t *head);
 size_t print_dlistint(const dlistint_t *h);
 size_t _print_dlistint_backward(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
